c6656fba012642966b4e64f023082c273c15c06c
jaxonyue <70416390+jaxonyue@users.noreply.github.com>
2023-09-12 17:45:41 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,9 +1,10 @@
 from main import median, rows, columns
 
-if __name__ == "__main__":
-    test_main()
-
 def test_main():
     assert median == 50564.5
     assert rows == 38
     assert columns == 5
+
+
+if __name__ == "__main__":
+    test_main()
